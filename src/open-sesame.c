1ab91ca772703a0a525f091cd43841e762594358
Mike Evans <mikee@saxicola.co.uk>
2018-06-01 08:59:32 +0100

    Clean up code.

--- src/open-sesame.c
+++ src/open-sesame.c
@@ -64,6 +64,8 @@ int card_present = 0;
 #define idlen 11 // Length of each card data string
 void *stored_rfid[nids][idlen] EEMEM;
 uint32_t used_rfids[10] EEMEM;
+unsigned char EEMEM pin[4];
+unsigned char pin[4];
 char * EOM = "###"; // End Of Message marker
 
 unsigned char messageBuf[4];
@@ -91,58 +93,11 @@ static void wait_new_card(void);
 static int delete_card(char idx);
 static void list_cards(void);
 static void check_button_status(void);
-static void sleepNow(void);
 
 
 /***********************************************************************
  * Functions/methods starts here
  **********************************************************************/
-/** Now is the time to set the sleep mode. In the Atmega8 datasheet
- * http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf on page 35
- * there is a list of sleep modes which explains which clocks and
- * wake up sources are available in which sleep modus.
- *
- * In the avr/sleep.h file, the call names of these sleep modus are to be found:
- *
- * The 5 different modes are:
- *     SLEEP_MODE_IDLE         -the least power savings
- *     SLEEP_MODE_ADC
- *     SLEEP_MODE_PWR_SAVE
- *     SLEEP_MODE_STANDBY
- *     SLEEP_MODE_PWR_DOWN     -the most power savings
- *
- *  the power reduction management <avr/power.h>  is described in
- *  http://www.nongnu.org/avr-libc/user-manual/group__avr__power.html
- */
-void
-sleepNow ()
-{
-
-
-    set_sleep_mode (SLEEP_MODE_IDLE);   // sleep mode is set here
-
-    sleep_enable ();        // enables the sleep bit in the mcucr register
-    // so sleep is possible. just a safety pin
-    /* Not in AtMega 8
-       power_adc_disable();
-       power_spi_disable();
-       power_timer0_disable();
-       power_timer1_disable();
-       power_timer2_disable();
-       power_twi_disable();
-     */
-    // Disable ADC
-    ACSR = (ACD << 1);
-
-    sleep_mode ();      // here the device is actually put to sleep!!
-
-    // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
-    sleep_disable ();       // first thing after waking from sleep:
-    // disable sleep...
-
-    //power_all_enable();
-
-}
 
 
 /***********************************************************************
@@ -152,7 +107,6 @@ sleepNow ()
 
 ISR (USART_RXC_vect)
 {
-    char rx;
     char *tmp;
     //static char RXBuf[MSG_LENGTH];
     //static int rxindex = 0;   // receive buffer index
@@ -238,7 +192,7 @@ int delete_card(char idx)
 {
     char *buffer;
     uint8_t n;
-    buffer = malloc (sizeof (char) * idlen);
+    buffer = "Card %d deleted.\r\n  ";
     list_cards();
     n = idx - '0';
     mode = READ;
@@ -247,7 +201,6 @@ int delete_card(char idx)
     USARTputs(buffer);
     USARTputs ("####");
     list_cards();
-    free(buffer);
     return idx;
 }
 
